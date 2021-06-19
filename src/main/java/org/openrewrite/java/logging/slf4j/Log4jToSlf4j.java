d9b1863f220a46b0fde6f2795c3adb24b23a965e
Aaron Gershman <aegershman@gmail.com>
2021-06-18 23:53:54 -0500

    lift and shift Log4jToSlf4j

--- src/main/java/org/openrewrite/java/logging/slf4j/Log4jToSlf4j.java
+++ src/main/java/org/openrewrite/java/logging/slf4j/Log4jToSlf4j.java
@@ -18,22 +18,41 @@ package org.openrewrite.java.logging.slf4j;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
-import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.*;
+import org.openrewrite.java.search.FindFields;
+import org.openrewrite.java.search.FindInheritedFields;
 import org.openrewrite.java.search.UsesType;
+import org.openrewrite.java.tree.Expression;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
+import org.openrewrite.java.tree.TypeUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
+ * Use of the traditional Log4J to SLF4J bridge can result in some
+ * loss of performance as the Log4j 2 Messages must be formatted
+ * before they can be passed to SLF4J.
+ * <p>
  * todo
  */
 public class Log4jToSlf4j extends Recipe {
+    private static final MethodMatcher GET_LOGGER_MATCHER = new MethodMatcher("org.apache.log4j.Logger getLogger(..)");
+    private static final MethodMatcher GET_LOGGER_WITH_MANAGER_MATCHER = new MethodMatcher("org.apache.log4j.LogManager getLogger(..)");
 
-    @Override
+    @Override // todo
     public String getDisplayName() {
         return "Log4jToSlf4j";
     }
 
-    @Override
+    @Override // todo
     public String getDescription() {
-        return "Log4jToSlf4j.";
+        return "TODO.";
     }
 
     @Override
@@ -43,10 +62,132 @@ public class Log4jToSlf4j extends Recipe {
 
     @Override
     protected TreeVisitor<?, ExecutionContext> getSingleSourceApplicableTest() {
-        return new UsesType<>("org.openrewrite.example.Example");
+        return new UsesType<>("org.apache.log4j.Logger");
     }
 
-    public static class Log4jToSlf4jVisitor extends JavaIsoVisitor<ExecutionContext> {
+    private class Log4jToSlf4jVisitor extends JavaIsoVisitor<ExecutionContext> {
+        private final List<MethodMatcher> logLevelMatchers = Stream.of("trace", "debug", "info", "warn", "error", "fatal")
+                .map(level -> "org.apache.log4j." + (level.equals("trace") ? "Logger" : "Category") +
+                        " " + level + "(..)")
+                .map(MethodMatcher::new)
+                .collect(Collectors.toList());
+
+        private final Stack<String> loggerField = new Stack<>();
+
+        @Override
+        public J.CompilationUnit visitCompilationUnit(J.CompilationUnit cu, ExecutionContext ctx) {
+            J.CompilationUnit c = super.visitCompilationUnit(cu, ctx);
+            doAfterVisit(new ChangeType("org.apache.log4j.Logger", "org.slf4j.Logger"));
+            return c;
+        }
+
+        @Override
+        public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+            String loggerFieldName = FindFields.find(classDecl, "org.apache.log4j.Logger")
+                    .stream()
+                    .findAny()
+                    .map(field -> field.getVariables().iterator().next().getSimpleName())
+                    .orElse(FindInheritedFields.find(classDecl, "org.apache.log4j.Logger")
+                            .stream()
+                            .findAny()
+                            .map(JavaType.Variable::getName)
+                            .orElse(null)
+                    );
+
+            Optional.ofNullable(loggerFieldName).ifPresent(loggerField::push);
+            J.ClassDeclaration c = super.visitClassDeclaration(classDecl, ctx);
+            Optional.ofNullable(loggerFieldName).ifPresent(field -> loggerField.pop());
+            return c;
+        }
+
+        @Override
+        public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+            J.MethodInvocation m = super.visitMethodInvocation(method, ctx);
+
+            if (GET_LOGGER_MATCHER.matches(m) || GET_LOGGER_WITH_MANAGER_MATCHER.matches(m)) {
+                String pattern = GET_LOGGER_MATCHER.matches(m) ? "org.apache.log4j.Logger getLogger(..)" : "org.apache.log4j.LogManager getLogger(..)";
+                doAfterVisit(new ChangeMethodTargetToStatic(pattern, "org.slf4j.LoggerFactory", null));
+            }
+
+            if (!loggerField.isEmpty()) {
+                for (MethodMatcher matcher : logLevelMatchers) {
+                    if (matcher.matches(m)) {
+                        if (m.getSimpleName().equals("fatal")) {
+                            doAfterVisit(new ChangeMethodName("org.apache.log4j.Category fatal(..)", "error"));
+                        }
+
+                        List<Expression> args = method.getArguments();
+                        if (!args.isEmpty()) {
+                            Expression message = args.iterator().next();
+                            if (!TypeUtils.isOfType(message.getType(), JavaType.Primitive.String)) {
+                                if (message.getType() instanceof JavaType.Class) {
+                                    m = m.withTemplate(
+                                            template("#{any(Object)}.toString()").build(),
+                                            m.getCoordinates().replaceArguments(),
+                                            message
+                                    );
+                                }
+                            } else if (message instanceof J.Binary) {
+                                MessageAndArguments literalAndArgs = concatenationToLiteral(message,
+                                        new MessageAndArguments("", new ArrayList<>()));
+
+                                StringBuilder template = new StringBuilder("\"" + literalAndArgs.message + "\"");
+                                literalAndArgs.arguments.forEach(arg -> template.append(", #{any()}"));
+                                m = m.withTemplate(
+                                        template(template.toString())
+                                                .build(),
+                                        m.getCoordinates().replaceArguments(),
+                                        literalAndArgs.arguments.toArray()
+                                );
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            return m;
+
+        }
+
+    }
+
+    private static class MessageAndArguments {
+        private String message;
+        private final List<Expression> arguments;
+
+        private MessageAndArguments(String message, List<Expression> arguments) {
+            this.message = message;
+            this.arguments = arguments;
+        }
+    }
+
+    private static MessageAndArguments concatenationToLiteral(Expression message, MessageAndArguments result) {
+        if (!(message instanceof J.Binary)) {
+            result.arguments.add(message);
+            return result;
+        }
+
+        J.Binary concat = (J.Binary) message;
+        if (concat.getLeft() instanceof J.Binary) {
+            concatenationToLiteral(concat.getLeft(), result);
+        } else if (concat.getLeft() instanceof J.Literal) {
+            result.message = ((J.Literal) concat.getLeft()).getValue() + result.message;
+        } else {
+            result.message = "{}" + result.message;
+            result.arguments.add(concat.getLeft());
+        }
+
+        if (concat.getRight() instanceof J.Binary) {
+            concatenationToLiteral(concat.getRight(), result);
+        } else if (concat.getRight() instanceof J.Literal) {
+            result.message += ((J.Literal) concat.getRight()).getValue();
+        } else {
+            result.message += "{}";
+            result.arguments.add(concat.getRight());
+        }
+
+        return result;
     }
 
 }
