c6a3a8c1358efec2ebb15a3bd4153d3676aac692
Tim te Beek <tim.te.beek@jdriven.com>
2022-08-25 22:56:39 +0200

    LoggersNamedForEnclosingClass recipe
    Fixes #65

---
+++ src/test/kotlin/org/openrewrite/java/logging/slf4j/LoggersNamedForEnclosingClassTest.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.logging.slf4j
+
+import org.junit.jupiter.api.Test
+import org.openrewrite.Issue
+import org.openrewrite.Recipe
+import org.openrewrite.java.JavaParser
+import org.openrewrite.java.JavaRecipeTest
+
+class LoggersNamedForEnclosingClassTest : JavaRecipeTest {
+    override val parser: JavaParser
+        get() = JavaParser.fromJavaVersion().classpath("slf4j-api").build()
+
+    override val recipe: Recipe
+        get() = LoggersNamedForEnclosingClass()
+
+    @Issue("https://github.com/openrewrite/rewrite-logging-frameworks/issues/65")
+    @Test
+    fun doNotUseStringFormat() = assertChanged(
+        before = """
+            import org.slf4j.Logger;
+            import org.slf4j.LoggerFactory;
+            class WrongClass {}
+            class A {
+                private final static Logger LOG = LoggerFactory.getLogger(WrongClass.class);
+            }
+        """,
+        after = """
+            import org.slf4j.Logger;
+            import org.slf4j.LoggerFactory;
+            class WrongClass {}
+            class A {
+                private final static Logger LOG = LoggerFactory.getLogger(A.class);
+            }
+        """
+    )
+}
