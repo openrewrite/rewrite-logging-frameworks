08c9ba48d509918d034f0efa203d97dbe6baef43
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-12-12 18:27:08 +0100

    Add basic JUL to Log4j API conversion
    This PR converts the most commonly used JUL methods to their
    Log4j API equivalents:
    
     * `Logger#getLogger`,
     * all `Logger#<logging_level` methods,
     * `Logger#entering`,
     * `Logger#exiting`.

---
+++ src/main/java/org/openrewrite/java/logging/log4j/ConvertJulEntering.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.logging.log4j;
+
+import static java.util.Objects.requireNonNull;
+import static org.openrewrite.Tree.randomId;
+
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.*;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.search.UsesMethod;
+import org.openrewrite.java.tree.*;
+import org.openrewrite.java.tree.J.Identifier;
+import org.openrewrite.java.tree.JavaType.Method;
+import org.openrewrite.java.tree.JavaType.Primitive;
+import org.openrewrite.marker.Markers;
+
+/**
+ * This recipe rewrites JUL's {@link java.util.logging.Logger#entering} method.
+ */
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class ConvertJulEntering extends Recipe {
+
+    private static final String METHOD_PATTERN = "java.util.logging.Logger entering(..)";
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class Builder {
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Rewrites JUL's Logger#entering method to Log4j API";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Replaces JUL's Logger#entering method calls to Log4j API Logger#traceEntry calls.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(new JavaVisitor<ExecutionContext>() {
+            @Override
+            public J visit(@Nullable Tree tree, ExecutionContext ctx) {
+                if (tree instanceof JavaSourceFile) {
+                    JavaSourceFile cu = (JavaSourceFile) requireNonNull(tree);
+                    return new UsesMethod<>(METHOD_PATTERN).visitNonNull(cu, ctx);
+                }
+                return super.visit(tree, ctx);
+            }
+        }, new EnteringMethodVisitor(new MethodMatcher(METHOD_PATTERN, false)));
+    }
+
+    private class EnteringMethodVisitor extends JavaIsoVisitor<ExecutionContext> {
+        private final MethodMatcher methodMatcher;
+
+        private EnteringMethodVisitor(MethodMatcher methodMatcher) {
+            this.methodMatcher = methodMatcher;
+        }
+
+        @Override
+        public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext ctx) {
+            return (J.MethodInvocation) visitMethodCall(super.visitMethodInvocation(method, ctx));
+        }
+
+        private J.MethodInvocation visitMethodCall(J.MethodInvocation m) {
+            if (methodMatcher.matches(m) && m.getMethodType() != null) {
+                final List<Expression> originalArgs = m.getArguments();
+                final int originalArgCount = originalArgs.size();
+                if (originalArgCount < 2 || 3 < originalArgCount) {
+                    throw new IllegalArgumentException("Unsupported Logger#entering method: " + m.getMethodType());
+                }
+                final List<Expression> modifiedArgs = new ArrayList<>();
+                final List<JavaType> modifiedTypes = new ArrayList<>();
+                final J.Literal nullString = buildNullString();
+                modifiedArgs.add(nullString);
+                modifiedTypes.add(Primitive.String);
+                if (originalArgCount > 2) {
+                    final JavaType varargType = JavaType.buildType("java.lang.Object[]");
+                    modifiedArgs.add(originalArgs.get(2));
+                    modifiedTypes.add(varargType);
+                }
+                final Identifier traceEntry = m.getName().withSimpleName("traceEntry");
+                final Method mt = m.getMethodType().withParameterTypes(modifiedTypes);
+                return m.withMethodType(mt)
+                        .withName(traceEntry)
+                        .withArguments(modifiedArgs)
+                        .withDeclaringType(mt.getDeclaringType()
+                                .withFullyQualifiedName("org.apache.logging.log4j.Logger"));
+            }
+            return m;
+        }
+    }
+
+    private static J.Literal buildNullString() {
+        return new J.Literal(randomId(), Space.EMPTY, Markers.EMPTY, null, "null", null, JavaType.Primitive.String);
+    }
+}
---
+++ src/test/java/org/openrewrite/java/logging/log4j/JulToLog4jTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.java.logging.log4j;
+
+import static org.openrewrite.java.Assertions.java;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+import org.openrewrite.test.TypeValidation;
+
+public class JulToLog4jTest implements RewriteTest {
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipeFromResource("/META-INF/rewrite/log4j.yml", "org.openrewrite.java.logging.log4j.JulToLog4j")
+          .parser(JavaParser.fromJavaVersion().classpath("log4j-api", "lombok"));
+    }
+
+    @Test
+    void loggerToLogManager() {
+        // language=java
+        rewriteRun(java("""
+          import java.util.logging.Logger;
+
+          class Test {
+              Logger log = Logger.getLogger("Test");
+          }
+          """, """
+          import org.apache.logging.log4j.LogManager;
+          import org.apache.logging.log4j.Logger;
+
+          class Test {
+              Logger log = LogManager.getLogger("Test");
+          }
+          """));
+    }
+
+    @Test
+    @DocumentExample
+    void simpleLoggerCalls() {
+        // language=java
+        rewriteRun(java("""
+          import java.util.logging.Logger;
+
+          class Test {
+              void method(Logger logger) {
+                logger.config("Hello");
+                logger.config(() -> "Hello");
+                logger.fine("Hello");
+                logger.fine(() -> "Hello");
+                logger.finer("Hello");
+                logger.finer(() -> "Hello");
+                logger.finest("Hello");
+                logger.finest(() -> "Hello");
+                logger.info("Hello");
+                logger.info(() -> "Hello");
+                logger.severe("Hello");
+                logger.severe(() -> "Hello");
+                logger.warning("Hello");
+                logger.warning(() -> "Hello");
+              }
+          }
+          """, """
+          import org.apache.logging.log4j.Logger;
+
+          class Test {
+              void method(Logger logger) {
+                logger.info("Hello");
+                logger.info(() -> "Hello");
+                logger.debug("Hello");
+                logger.debug(() -> "Hello");
+                logger.trace("Hello");
+                logger.trace(() -> "Hello");
+                logger.trace("Hello");
+                logger.trace(() -> "Hello");
+                logger.info("Hello");
+                logger.info(() -> "Hello");
+                logger.error("Hello");
+                logger.error(() -> "Hello");
+                logger.warn("Hello");
+                logger.warn(() -> "Hello");
+              }
+          }
+          """));
+    }
+
+    @Test
+    void changeLombokLogAnnotation() {
+        // language=java
+        rewriteRun(spec -> spec.typeValidationOptions(TypeValidation.builder()
+          .identifiers(false)
+          .methodInvocations(false)
+          .build()), java("""
+          import lombok.extern.java.Log;
+
+          @Log
+          class Test {
+              void method() {
+                  log.info("uh oh");
+              }
+          }
+          """, """
+          import lombok.extern.log4j.Log4j2;
+
+          @Log4j2
+          class Test {
+              void method() {
+                  log.info("uh oh");
+              }
+          }
+          """));
+    }
+}
