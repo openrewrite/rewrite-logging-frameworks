886d91498115141ca51ae927dda8ff06fc03270b
Tim te Beek <timtebeek@gmail.com>
2024-02-09 16:55:38 +0000

    refactor: RewriteTest classes should not be public
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.java.recipes.RewriteTestClassesShouldNotBePublic?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/java/logging/slf4j/ChangeLogLevelTest.java
+++ src/test/java/org/openrewrite/java/logging/slf4j/ChangeLogLevelTest.java
@@ -23,7 +23,7 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
-public class ChangeLogLevelTest implements RewriteTest {
+class ChangeLogLevelTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
