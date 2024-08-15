f819ea73f688c33a43de5738a321f7d69349efec
Jonathan Schnéider <jkschneider@gmail.com>
2024-08-15 04:20:01 +0000

    Migrate to JSpecify from OpenRewrite JSR-305 meta-annotations
    Use this link to re-run the recipe: https://app.moderne.io/builder/gW3xaSLWV?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/openrewrite/java/logging/SystemOutToLogging.java
+++ src/main/java/org/openrewrite/java/logging/SystemOutToLogging.java
@@ -17,8 +17,8 @@ package org.openrewrite.java.logging;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import org.jspecify.annotations.Nullable;
 import org.openrewrite.*;
-import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.*;
 import org.openrewrite.java.search.FindFieldsOfType;
 import org.openrewrite.java.search.UsesMethod;
