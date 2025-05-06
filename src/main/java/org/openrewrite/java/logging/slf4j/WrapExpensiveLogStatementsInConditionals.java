167595a9cf2e0468bc4b7020b4cf6139434511c9
JohannisK <johan.kragt@moderne.io>
2025-05-06 12:08:33 +0200

    Make inexpensive log statements less noisy (#218)
    * Scoped autoFormat better

--- src/main/java/org/openrewrite/java/logging/slf4j/WrapExpensiveLogStatementsInConditionals.java
+++ src/main/java/org/openrewrite/java/logging/slf4j/WrapExpensiveLogStatementsInConditionals.java
@@ -27,6 +27,7 @@ import org.openrewrite.java.tree.*;
 import org.openrewrite.marker.Markers;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
@@ -87,7 +88,7 @@ public class WrapExpensiveLogStatementsInConditionals extends Recipe {
                                 .withThenPart(m.withPrefix(m.getPrefix().withWhitespace("\n" + m.getPrefix().getWhitespace().replace("\n", ""))))
                                 .withPrefix(m.getPrefix().withComments(emptyList()));
                         visitedBlocks.add(id);
-                        return autoFormat(if_, ctx);
+                        return if_;
                     }
                 }
             }
@@ -125,11 +126,11 @@ public class WrapExpensiveLogStatementsInConditionals extends Recipe {
         public J.Block visitBlock(J.Block block, ExecutionContext ctx) {
             J.Block b = super.visitBlock(block, ctx);
             if (blockIds.contains(b.getId())) {
-                StatementAccumulator acc = new StatementAccumulator();
+                StatementAccumulator acc = new StatementAccumulator((J j) -> autoFormat(j, ctx, getCursor()));
                 for (Statement statement : b.getStatements()) {
                     acc.push(statement);
                 }
-                return autoFormat(b.withStatements(acc.pull()), ctx);
+                return b.withStatements(acc.pull());
             }
             return b;
         }
@@ -149,10 +150,15 @@ public class WrapExpensiveLogStatementsInConditionals extends Recipe {
      */
     private static class StatementAccumulator {
 
-        AccumulatorKind accumulatorKind = AccumulatorKind.NONE;
-        List<Statement> statements = new ArrayList<>();
-        List<Statement> logStatementsCache = new ArrayList<>();
-        J.@Nullable If ifCache = null;
+        private final Function<J, J> formatter;
+        private final List<Statement> statements = new ArrayList<>();
+        private final List<Statement> logStatementsCache = new ArrayList<>();
+        private AccumulatorKind accumulatorKind = AccumulatorKind.NONE;
+        private J.@Nullable If ifCache = null;
+
+        public StatementAccumulator(Function<J, J> formatter) {
+            this.formatter = formatter;
+        }
 
         public void push(Statement statement) {
             AccumulatorKind newKind = getKind(statement);
@@ -228,7 +234,8 @@ public class WrapExpensiveLogStatementsInConditionals extends Recipe {
             if (ifCache == null) {
                 statements.addAll(logStatementsCache);
             } else {
-                statements.add(ifCache.withThenPart(new J.Block(randomId(), Space.EMPTY, Markers.EMPTY, JRightPadded.build(false), logStatementsCache.stream().map(JRightPadded::build).collect(Collectors.toList()), Space.EMPTY)));
+                J.If anIf = ifCache.withThenPart(new J.Block(randomId(), Space.EMPTY, Markers.EMPTY, JRightPadded.build(false), logStatementsCache.stream().map(JRightPadded::build).collect(Collectors.toList()), Space.EMPTY));
+                statements.add((Statement) formatter.apply(anIf));
             }
             logStatementsCache.clear();
             ifCache = null;
