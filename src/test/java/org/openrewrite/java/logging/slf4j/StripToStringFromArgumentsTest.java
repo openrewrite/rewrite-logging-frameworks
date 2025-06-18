adb63563dfb35cfc463ddd3ebe65663f5b94a03f
Pierre Delagrave <pierre@moderne.io>
2025-06-18 10:07:29 -0400

    Licenses

--- src/test/java/org/openrewrite/java/logging/slf4j/StripToStringFromArgumentsTest.java
+++ src/test/java/org/openrewrite/java/logging/slf4j/StripToStringFromArgumentsTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.java.logging.slf4j;
 
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,7 +28,7 @@ import java.util.List;
 
 import static org.openrewrite.java.Assertions.java;
 
-public class StripToStringFromArgumentsTest implements RewriteTest {
+class StripToStringFromArgumentsTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         spec.recipe(new StripToStringFromArguments()).parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(), "slf4j-api-2.1.+"));
